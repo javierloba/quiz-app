adfa0bd89fd652516120de69a6ac6c1f942453d5
Javier Loba <30801844+javierloba@users.noreply.github.com>
2021-12-16 17:47:43 +0100

    Created logic for textfields & start button

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,18 +1,42 @@
-import { MenuItem, TextField } from "@material-ui/core"
 import "./Home.css"
-import Categories, { } from '../../Data/Categories'
+import { Button, MenuItem, TextField } from "@material-ui/core"
+import Categories from '../../Data/Categories'
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import ErrorMessage from "../../components/ErrorMessage/ErrorMessage"
 
-const Home = () => {
+const Home = ({ name, setName, fetchQuestions }) => {
+
+    const [category, setCategory] = useState("");
+    const [difficulty, setDifficulty] = useState("");
+    const [error, setError] = useState(false);
+
+    const navigate = useNavigate()
+
+    const handleSubmit = () => {
+        if(!category||!difficulty||!name) {
+            setError(true);
+            return;
+        } else {
+            setError(false)
+            fetchQuestions(category, difficulty);
+            navigate("/quiz");
+        }
+    }
+
     return (
         <div className="content">
             <div className="settings">
                 <span style={{ fontSize: 30 }} >Quiz settings</span>
                 
                 <div className="settings_select">
+                    {error && <ErrorMessage>Please fill all the fields</ErrorMessage>}
+
                     <TextField 
                     style={{marginBottom: 25}}
                     label='Enter your name' 
                     variant='outlined' 
+                    onChange={(e) => setName(e.target.value)}
                     />
 
                     <TextField
@@ -20,6 +44,8 @@ const Home = () => {
                     select
                     label='Select category'
                     variant='outlined' 
+                    onChange={(e) => setCategory(e.target.value)}
+                    value={category}
                     >
                         {
                             Categories.map((cat) => (
@@ -33,8 +59,8 @@ const Home = () => {
                     <TextField
                     select
                     label='Select Difficulty'
-                    value={difficulty}
                     onChange={(e) => setDifficulty(e.target.value)}
+                    value={difficulty}
                     variant='outlined'
                     style={{ marginBottom: 30 }}
                     >
@@ -48,6 +74,15 @@ const Home = () => {
                             Hard
                         </MenuItem>
                     </TextField>
+
+                    <Button 
+                    variant='contained' 
+                    color='primary' 
+                    size='large'
+                    onClick={handleSubmit}
+                    >
+                        Start Quiz
+                    </Button>
 
                 </div>
             </div>
