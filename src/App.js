a5300977b4d97f18db18c1584a7477ba9bcede94
Javier Loba <30801844+javierloba@users.noreply.github.com>
2021-12-16 12:07:39 +0100

    Created basic structure
    Created a basic structure with header, footer, background image & styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
 
 function App() {
   return (
-    <div className="App">
-      hola
-    </div>
+    <BrowserRouter>
+      <div className="app" style={{backgroundImage: 'url(./quiz-background.jpeg)'}}>
+        <Header />
+      </div>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div style={{ textAlign:'center', marginBottom:10 }}>
+            Made by{" "}
+            <a href="https://github.com/javierloba" style={{ cursor: "pointer" }}>
+                Javier Loba
+            </a>
+        </div>
+    )
+}
+
+export default Footer
