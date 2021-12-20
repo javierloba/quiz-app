bcbea864f7aff98590758f6d1c9a73936ea6e8d2
Javier Loba <30801844+javierloba@users.noreply.github.com>
2021-12-20 16:11:52 +0100

    Created login & styles for options in Questions component

--- src/components/Questions/Questions.js
+++ src/components/Questions/Questions.js
@@ -1,13 +1,52 @@
 import { useState } from "react"
+import ErrorMessage from "../ErrorMessage/ErrorMessage"
+import './Questions.css'
 
-const Questions = () => {
+const Questions = ({ 
+    currQues, setCurrQues, questions, options, correct, setScore, score, setQuestions 
+}) => {
 
     const [selected, setSelected] = useState()
     const [error, setError] = useState(false)
 
+    const handleSelect = (i) => {
+        if(selected === i && selected === correct) {
+            return "select";
+        } else if(selected === i && selected != correct ) {
+            return "wrong"
+        } else if(i === correct) {
+            return "select"
+        }
+    }
+
+    const handleCheck = (i) => {
+        setSelected(i);
+        if (i === correct) setScore(score + 1);
+        setError(false)
+    }
+
     return (
-        <div>
-            
+        <div className="question">
+            <h1>Question {currQues +1}</h1>
+            <div className="singleQuestion">
+                <h2>{ questions[currQues].question }</h2>
+
+                <div className="options">
+                    { error && <ErrorMessage>{error}</ErrorMessage> }
+                    {options &&
+                        options.map((i) => (
+                            <button 
+                            onClick={() => handleCheck(i)}
+                            className={`singleOption ${selected && handleSelect(i)}`}
+                            key={i}
+                            disabled={selected}
+                            >
+                                {i}
+                            </button>
+                        ))
+                    }
+                </div>
+            </div>
         </div>
     )
 }
