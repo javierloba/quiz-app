ff7073aa8f738cd0960099d66b0cd6b56ff9fb49
Javier Loba <30801844+javierloba@users.noreply.github.com>
2021-12-20 13:14:30 +0100

    Quiz page logic

--- src/Pages/Quiz/Quiz.js
+++ src/Pages/Quiz/Quiz.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 
-const Quiz = () => {
+const Quiz = ({ name, score, questions, setQuestions, setScore }) => {
+
+    const [options, setOptions] = useState()
+    const [currQues, setCurrQues] = useState(0)
+
+    useEffect(() => {
+        setOptions(
+            questions && 
+            handleShuffle([
+                questions[currQues]?.correct_answer,
+                ...questions[currQues]?.incorrect_answers,
+            ])
+        );
+
+    }, [questions])
+
+    console.log(options)
+
+    const handleShuffle = (optionss) => {
+        return optionss.sort(()=> Math.random()-0.5)
+    }
+
     return (
         <div>
-            Quiz
+            {name}
         </div>
     )
 }
